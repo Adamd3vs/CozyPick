03adcfb0ef5fe787ebeb8a3a674c4dcea356e387
debug_Adam <mirjalolesanovmain@gmail.com>
2025-11-05 18:02:52 +0900

    Fix import statement for PropertySchema

--- apps/cozypick-api/src/components/notification/notification.module.ts
+++ apps/cozypick-api/src/components/notification/notification.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import NotificationSchema from '../../schemas/Notification.model';
-import PropertySchema from '../../schemas/Property.model'; // ← shu to‘g‘ri yo‘lmi? (pastga qarang)
+import PropertySchema from '../../schemas/Property.model'; 
 import { MemberModule } from '../member/member.module';
 import { NotificationService } from './notification.service';
 import { NotificationResolver } from './notification.resolver';
